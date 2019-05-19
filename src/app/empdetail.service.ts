b44a934cbd2101e187d4314a699b91fe5170f083
Dinesh Panchavarnam <Dinesh.Panchavarnam@jbhunt.com>
2019-05-19 18:28:07 -0500

    URL Configured.

--- src/app/empdetail.service.ts
+++ src/app/empdetail.service.ts
@@ -29,27 +29,27 @@ export class EmpdetailService {
   }
 
   getmydet(): Observable<Empdetail> {
-    let url="http://localhost:6677/user/"+this.empid;
+    let url="http://localhost:9090/employee/"+this.empid;
       return this.http.get<any>(url);
      }
 
   getmgrdet(): Observable<Empdetail> {
-    let url="http://localhost:6677/user/mgr/"+this.empid;
+    let url="http://localhost:9090/employee/mgr/"+this.empid;
       return this.http.get<any>(url);
        }
   
   getlvedet(): Observable<Emplvedet[]> {
-    let url="http://localhost:6677/user/leave/"+this.empid;
+    let url="http://localhost:9090/employee/leave/"+this.empid;
        return this.http.get<Emplvedet[]>(url);
      }
   
   getrptlvedet(): Observable<Emplvedet[]> {
-    let url="http://localhost:6677/user/rptleave/"+this.empid;
+    let url="http://localhost:9090/employee/rptleave/"+this.empid;
        return this.http.get<Emplvedet[]>(url);
      }
 
   emplveSave(emplvedet : Emplvedet): Observable<String> {
-      let url="http://localhost:6677/user/leave/create";
+      let url="http://localhost:9090/employee/leave/create";
       return this.http.post<any>(url,emplvedet,httpOptions)
       .pipe(
            catchError(err => this.handleError(err))
@@ -59,7 +59,7 @@ export class EmpdetailService {
       throw new Error("Error postng data!!." + arg0);
     }
   aprvelve(emplvedet : Emplvedet): Observable<String> {
-      let url="http://localhost:6677/user/leave/approve";
+      let url="http://localhost:9090/employee/leave/approve";
       return this.http.post<any>(url,emplvedet,httpOptions)
       .pipe(
            catchError(err => this.handleError1(err))
